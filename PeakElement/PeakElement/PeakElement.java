8863eaf4f7d0e06749a9c7d409d16f135858c140
Elija Amponsa <amponseli@gmail.com>
2022-07-18 23:18:24 -0400

    find the peak element

---
+++ PeakElement/PeakElement/PeakElement.java
@@ -0,0 +1,17 @@
+package PeakElement;
+
+public class PeakElement{
+    public static int peak(int number[], int size){
+        //create a temp valuable to hold the element in the array
+        int count = number[0];
+        // loop to gos thought the array.
+        for(int i = 0; i < size; i++){
+            if(count[0] <= number[i]);
+        }
+        
+        // compair the temp element to next element to find the bigest element aroud small element.
+    }
+    public static void main(String[] args) {
+        
+    }
+}
