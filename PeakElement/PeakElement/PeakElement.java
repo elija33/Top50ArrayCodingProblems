b68dfeea25a7874771ee2998410a27e349fdc680
Elija Amponsa <amponsaeli@gmail.com>
2022-07-28 00:21:50 -0400

    Update PeakElement.java

--- PeakElement/PeakElement/PeakElement.java
+++ PeakElement/PeakElement/PeakElement.java
@@ -1,17 +1,18 @@
 package PeakElement;
 
 public class PeakElement{
-    public static int peak(int number[], int size){
+    public static int peak(int number[]){
         //create a temp valuable to hold the element in the array
         int count = number[0];
         // loop to gos thought the array.
-        for(int i = 0; i < size; i++){
+        for(int i = 0; i < number.length -1; i++){
             if(count[0] <= number[i]);
         }
         
         // compair the temp element to next element to find the bigest element aroud small element.
     }
     public static void main(String[] args) {
+        int number[] = {5, 20, 40, 30, 20, 50, 60};
+        System.out.println(peak(number));
         
-    }
 }
