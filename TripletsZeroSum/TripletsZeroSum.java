b6783f35ded7ca1fb215f5f6fcf0937995190f66
elija33 <amponsaeli@gmail.com>
2022-08-27 22:50:02 -0400

    Triplets Zero Sum

---
+++ TripletsZeroSum/TripletsZeroSum.java
@@ -0,0 +1,19 @@
+package TripletsZeroSum;
+
+public class TripletsZeroSum {
+    public static int TripletsZeroSum(int number[]){
+        for(int i = 0; i < number.length; i++){
+            for(int j = i + 1; j < number.length; j++){
+                for(int k = j + 1; k < number.length; k++){
+                    if(number[i] + number[j] + number[k] == 0){
+                        System.out.println(number[i]+ " " + number[j] + " " + number[k]);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
