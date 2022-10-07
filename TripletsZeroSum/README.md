c99fd0b4c4412e9e0b1947cea222703ff9a4637e
Elija33 <amponsaeli@gmail.com>
2022-10-07 16:55:36 -0400

    Triplets Zero Sum

--- TripletsZeroSum/README.md
+++ TripletsZeroSum/README.md
@@ -1,2 +1,13 @@
 # Top50ArrayCodingProblems
+
+                                        Three Number Sum Problem Statement
+
+Given an array of integers, find all triplets in the array that sum up to a given target value.
+In other words, given an array arr and a target value target, return all triplets a, b, c such that a + b + c = 0.
  
+ Example 1:
+
+Input array: [7, 12, 3, 1, 2, -6, 5, -8, 6]
+Target sum: 0
+
+Output: [[2, -8, 6], [3, 5, -8], [1, -6, 5]]
--- TripletsZeroSum/TripletsZeroSum.java
+++ TripletsZeroSum/TripletsZeroSum.java
@@ -1,19 +1,21 @@
 package TripletsZeroSum;
 
 public class TripletsZeroSum {
-    public static int TripletsZeroSum(int number[]){
-        for(int i = 0; i < number.length; i++){
-            for(int j = i + 1; j < number.length; j++){
-                for(int k = j + 1; k < number.length; k++){
-                    if(number[i] + number[j] + number[k] == 0){
-                        System.out.println(number[i]+ " " + number[j] + " " + number[k]);
+    public static int TripletsZeroSum(int number[]) {
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        System.out.println(number[i] + " " + number[j] + " " + number[k]);
                     }
                 }
             }
         }
         return -1;
     }
+
     public static void main(String[] args) {
-        
+        int number[] = { 2, 4, 5, 6, 8, 9, 7, 20, 10, -6 };
+        System.out.println(TripletsZeroSum(number));
     }
 }
