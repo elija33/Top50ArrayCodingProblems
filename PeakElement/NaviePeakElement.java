fcce94f57d8493f811eff35945f83568a0a816c5
Elija33 <amponsaeli@gmail.com>
2022-10-22 23:31:24 -0400

    Peak Element

---
+++ PeakElement/NaviePeakElement.java
@@ -0,0 +1,18 @@
+package PeakElement;
+
+public class NaviePeakElement {
+    public static int peakElement(int number[]) {
+        for (int i = 0; i < number.length - 1; i++) {
+            if (number[i] > number[i + 1]) {
+                return number[i];
+            }
+        }
+        return number[number.length - 1];
+    }
+
+    public static void main(String[] args) {
+        int[] number = { 2, 3, 1, 4, 7, 5 };
+        int total = peakElement(number);
+        System.out.println(total);
+    }
+}
