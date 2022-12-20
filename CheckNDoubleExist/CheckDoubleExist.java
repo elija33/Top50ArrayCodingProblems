0fc0ef160f383efc38be0c3013186bdbaf4da57d
Elija33 <amponsaeli@gmail.com>
2022-12-20 15:40:08 -0500

    Still working on it

--- CheckNDoubleExist/CheckDoubleExist.java
+++ CheckNDoubleExist/CheckDoubleExist.java
@@ -2,15 +2,20 @@ package CheckNDoubleExist;
 
 public class CheckDoubleExist {
     public static boolean checkDouble(int number[]) {
-        for (int i = 0; i < number.length; i++)
-            if (number[i] == number[i + 1] * number[i + 1 + 1])
-                return true;
-
+        for (int i = 0; i < number.length; i++) {
+            for (int j = 0; j < number.length; j++) {
+                if (i != j) {
+                    if (number[i] == 2 * number[j]) {
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 
     public static void main(String[] args) {
-        int number[] = { 3, 1, 7, 11 };
+        int number[] = { 0, 0 };
         System.out.println(checkDouble(number));
     }
 }
---
+++ ValidMountainArray/MountainArray.java
@@ -0,0 +1,7 @@
+package ValidMountainArray;
+
+public class MountainArray {
+    public static void main(String[] args) {
+
+    }
+}
